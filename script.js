f7c2130ac939767c108c4cb3ba002ba6cd264120
Łukasz Sadowski <Lukasz.sadowski2605@gmail.com>
2023-07-31 20:49:48 +0200

    ending header on all devices

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const modal = document.querySelector(".header__modal");
 const burgerBtn = document.querySelector(".header__hamburger__box");
 const closeBtn = document.querySelector(".close-btn");
+const mobileNavLink = document.querySelectorAll(
+  ".header__modal__list__item__link"
+);
 
 const showModal = function () {
   modal.classList.toggle("hide");
@@ -11,3 +14,4 @@ const closeModal = function () {
 
 burgerBtn.addEventListener("click", showModal);
 closeBtn.addEventListener("click", closeModal);
+mobileNavLink.forEach((el) => el.addEventListener("click", closeModal));
